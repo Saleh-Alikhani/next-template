4acedf7e56c4be5d253729a39c1bf881e8e3d7f4
Saleh Alikhani <salehalikhani@gmail.com>
2024-04-04 15:18:41 +0330

    husky test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,5 +4,6 @@ import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log('test');
+  console.log('test');
   return <Component {...pageProps} />;
 }
